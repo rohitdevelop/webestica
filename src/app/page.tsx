2c7878be1d078c7ca11bb31146fd2da35bfbc130
Rohit singh <rohitdev124421@gmail.com>
2025-11-01 13:33:55 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Webestica from '../component/Webestica'
+export default function Home() {
+  return (
+    <div >
+ <Webestica />
+ 
+     </div>
+  );
+}
---
+++ src/component/Webestica.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Webestica = () => {
+  return (
+    <div>
+      <h1>hello bro</h1>
+    </div>
+  )
+}
+
+export default Webestica
