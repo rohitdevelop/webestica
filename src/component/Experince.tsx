696fdf984b472d7a8b9cc71f85c0fcef7aa71e59
Rohit singh <rohitdev124421@gmail.com>
2025-11-03 16:08:44 +0530

    done

--- src/component/Experince.tsx
+++ src/component/Experince.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { motion, VariableTransitions } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import React, { FC } from "react";
 
 // --- 1. Type Definitions (Interfaces) ---
